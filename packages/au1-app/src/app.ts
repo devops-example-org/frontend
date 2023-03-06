2fd464d40bf4ab3c5b29d6c32b3e2f9ee551eb1f
David Kossoglyad <david@logoui.co.il>
2023-03-06 12:10:01 +0200

    feat(au1-app): add the router and home route

--- packages/au1-app/src/app.ts
+++ packages/au1-app/src/app.ts
@@ -1,3 +1,19 @@
+import { PLATFORM } from 'aurelia-framework';
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
+
+  private router: Router;
+
   public message: string;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.map([
+      { route: ['', 'home'],          moduleId: PLATFORM.moduleName('routes/home/index'),     name: 'home',    nav: true,   title: 'Home' },
+      //{ route: '/contacts',  moduleId: './contacts', name:'contacts', nav: true,   title: 'Contacts' }
+    ]);
+
+    this.router = router;
+  }
 }
