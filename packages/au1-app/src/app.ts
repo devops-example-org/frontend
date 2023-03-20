a3d4dce2526614ea06f0d4944266c10fb7068cf1
David Kossoglyad <david@logoui.co.il>
2023-03-20 16:27:34 +0200

    feat(au1-app): implement the integration
    issue: #15 pr: #16

--- packages/au1-app/src/app.ts
+++ packages/au1-app/src/app.ts
@@ -11,7 +11,8 @@ export class App {
     config.title = 'Contacts';
     config.map([
       { route: ['', 'home'],          moduleId: PLATFORM.moduleName('routes/home/index'),     name: 'home',    nav: true,   title: 'Home' },
-      { route: '/app-module-one',  moduleId: PLATFORM.moduleName('routes/app-module-one/index'), name:'app-module-one', nav: true,   title: 'The App Module #1' }
+      { route: '/app-module-one',  moduleId: PLATFORM.moduleName('routes/app-module-one/index'), name:'app-module-one', nav: true,   title: 'The App Module #1' },
+      { route: '/monday-integration',  moduleId: PLATFORM.moduleName('routes/monday-integration/index'), name:'monday-integration', nav: true,   title: 'The Monday.com Integration' }
     ]);
 
     this.router = router;
